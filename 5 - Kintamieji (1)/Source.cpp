754b23f7c62da84e5e2a7760368c7a458c21e305
Ieva <ieva.jaksaityte@gmail.com>
2016-10-24 18:11:48 +0300

    pridėti pamokų kodai

---
+++ 5 - Kintamieji (1)/Source.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	// deklaracija ir inicializacija vienoje eilutėje
+	int skaicius = 10;
+	int Skaicius = 20;
+	float skaicius_per_kabl_1 = 14.95334;
+	double skaicius_per_kabl_2 = 5.696464548;
+	char raide = 'a';
+	string tekstas = "cia yra kazkoks sakinys";
+	bool tiesa_netiesa = true; // false
+
+	// deklaracija
+	int amzius;
+	float kitas;
+	double trecias;
+	char kita_raide;
+	string mano_tekstas;
+	bool tiesa;
+
+	// inicializacija
+	amzius = 21;
+	kitas = 56.9415;
+	trecias = 656.22548654654;
+	kita_raide = 'b';
+	mano_tekstas = "habhsbfhbshgbdhgb";
+	tiesa = true;
+
+	// vienos eilutės komentaras
+
+	/* ilgesnis komentaras,
+	per kelias eilutes
+	rašomas taip */
+
+	/*
+
+	UŽDUOTIS
+
+	sukurti šiuos kintamuosius ir
+	priskirti jiems pasirinktas reikšmes:
+	- studento vardas, pavardė
+	- studento amžius
+	- studento ūgis
+	- dėstytojo komentaras apie studentą
+	- studento pažymių vidurkis
+	- studento įvertinimas raide
+	- ar studentas yra išlaikęs egzaminą?
+	*/
+
+	return 0;
+}
---
+++ 6 - Kintamieji (2)/Source.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	// reikšmės atnaujinimas
+	int skaicius = 10;
+	skaicius = 5;
+
+	// tų pačių tipų kintamųjų kūrimas per kablelį
+	int a, b, c; // int a; int b; int c;
+	int d = 14,
+		e = 10;
+	
+	// reikšmės kopijavimas į kitą kintamąjį 
+	a = d;
+	b = a;
+	c = e;
+
+	// dvigubas reikšmės priskyrimas
+	int f, l;
+	f = l = 8; // f = 8; l = 8;
+
+	/*
+	galimi pavadinimai:
+	skaicius1
+	skaicius
+	Skaicius
+	skaicius_1
+	zodis_zodis
+	_zodis
+
+	negalimi pavadinimai:
+	8_bsfbsd
+	as@%j&&jsf##sjfj
+	*/
+
+	/*
+	
+	UŽDUOTIS
+
+	Sukurti tris vienodo tipo kintamuosius,
+	tik vieną kartą nurodant kintamojo tipą.
+	
+	*/
+
+	return 0;
+}
