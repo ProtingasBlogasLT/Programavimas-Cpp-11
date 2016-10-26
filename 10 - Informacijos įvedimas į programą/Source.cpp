6ecceef016c0eb82e0e4b700c5586a5ddf77a97d
Ieva <ieva.jaksaityte@gmail.com>
2016-10-27 00:58:41 +0300

    pridėti pamokų kodai

---
+++ 10 - Informacijos įvedimas į programą/Source.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	// lietuviškos raidės konsolėje
+	setlocale(LC_ALL, "Lithuania");
+
+	int a, b;
+	cout << "įveskite skaičių a: ";
+	cin >> a; // įvedimas į kintamąjį
+	cout << "įveskite skaičių b: ";
+	cin >> b;
+	cout << a << " " << b << endl;
+
+	int c, d;
+	cout << "įveskite du skaičius: ";
+	cin >> c >> d; // atsirinks per tarpus ir naujas eilutes
+	cout << c << " " << d << endl;
+
+	float sk;
+	cout << "iveskite skaiciu per kableli (reikia rasyti taska): ";
+	cin >> sk;
+	cout << sk << endl;
+
+	return 0;
+}
---
+++ 11 - Kelių žodžių įvedimas į vieną kintamąjį/Source.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string vardas_pavarde;
+	cout << "iveskite varda ir pavarde: ";
+	getline(cin, vardas_pavarde);
+	cout << vardas_pavarde << endl;
+	/*
+	jeigu neveiks antras ivedimas, ismeginti:
+	cin.ignore();
+	cin.clear();
+	*/
+	string sakinys;
+	cout << "iveskite sakini: ";
+	getline(cin, sakinys);
+	cout << sakinys << endl;
+
+	return 0;
+}
