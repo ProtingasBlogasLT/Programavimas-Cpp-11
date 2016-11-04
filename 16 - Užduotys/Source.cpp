95d4bcfa8268df3e6f11b0a5aaf7fd2d920f10b8
Ieva <ieva.jaksaityte@gmail.com>
2016-11-04 18:13:34 +0200

    pridėti pamokų kodai ir užduotys

---
+++ 16 - Užduotys/Source.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int a;
+	string b;
+	cout << "iveskite ....: ";
+	cin >> a;
+
+	if (a > 10)
+	{
+		cout << "kazkoks isvedimas" << endl;
+		cin >> b;
+	}
+
+	return 0;
+}
