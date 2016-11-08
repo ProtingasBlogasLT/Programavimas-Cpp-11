34782c2df3006e8b6ea3b178ad49e125de86ba19
Ieva <ieva.jaksaityte@gmail.com>
2016-11-08 12:52:42 +0200

    pridėti pamokų kodai

---
+++ 17 - Patikrinimo sąlyga inline if/Source.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 5, b = 3;
+
+	if (a > b)
+	{
+		cout << "daugiau" << endl;
+	}
+	else
+	{
+		cout << "mažiau" << endl;
+	}
+
+	cout << (a > b ? "daugiau" : "mažiau") << endl;
+
+	/*if (a > b)
+	{
+		b = a;
+	}*/
+
+	cout << a << " " << b << endl;
+	b = (a > b ? a : b);
+	cout << a << " " << b << endl;
+
+	return 0;
+}
---
+++ 18 - Patikrinimo sąlyga switch/Source.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 2;
+
+	if (a == 1)
+	{
+		//..
+	}
+	else if (a == 2)
+	{
+		//..
+	}
+	else
+	{
+		//..
+	}
+
+	switch (a)
+	{
+	case 1:
+		cout << 1 << endl;
+		break;
+	case 2:
+		cout << 2 << endl;
+		break;
+	default:
+		cout << "ivyko klaida" << endl;
+	}
+
+	cout << "pasirinkite:" << endl;
+	cout << "1 - laptopas" << endl;
+	cout << "2 - stacionarus" << endl;
+	int pasirinkimas;
+	cin >> pasirinkimas;
+
+	switch (pasirinkimas)
+	{
+	case 1: cout << "laptopas" << endl; break;
+	case 2: cout << "stacionarus" << endl; break;
+	default: cout << "blogas pasirinkimas" << endl;
+	}
+
+	cin >> pasirinkimas;
+
+	if (pasirinkimas == 1)
+	{
+		cout << "laptopas" << endl;
+	}
+	if (pasirinkimas == 2)
+	{
+		cout << "stacionarus" << endl;
+	}
+	else
+	{
+		cout << "blogas pasirinkimas" << endl;
+	}
+
+	return 0;
+}
