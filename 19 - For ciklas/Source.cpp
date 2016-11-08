7018b78a27d44900b8ba06cce2cba616ee5fc1bb
Ieva <ieva.jaksaityte@gmail.com>
2016-11-08 21:21:52 +0200

    pridėti pamokų kodai ir užduotys

---
+++ 19 - For ciklas/Source.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "pirmas ciklas" << endl;
+	// for (nuo ko pradeda; salyga; iteracija kievieno ciklo vykdymo metu)
+	for (int i = 0; i < 10; i++)
+	{
+		cout << i << " " << i + 1 << endl;
+	}
+
+	cout << "antras ciklas" << endl;
+	int i;
+	for (i = 1; i <= 10; i++)
+	{
+		cout << i << endl;
+	}
+
+	cout << "trecias ciklas" << endl;
+	for (int i = 0; i < 50; i+=5)
+	{
+		cout << i << endl;
+	}
+
+	cout << "ketvirtas ciklas" << endl;
+	for (int i = 10; i > 0; i--)
+	{
+		cout << i << endl;
+	}
+
+	return 0;
+}
---
+++ 20 - For ciklo pavyzdžiai/Source.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	// 1 pavyzdys
+
+	int suma = 0;
+
+	// [1-100]
+	for (int i = 1; i <= 100; i++)
+	{
+		suma += i;
+	}
+
+	cout << "[0-100] skaičių suma: " << suma << endl;
+
+	// 2 pavyzdys
+
+	int a = 10;
+	string tekstas = "tekstas, kuris bus isvedamas daug kartu";
+	
+	for (int i = 0; i < a; i++)
+	{
+		cout << i + 1 << tekstas << endl;
+	}
+
+	// 3 pavyzdys
+
+	int suma2 = 0;
+
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 4 == 0)
+		{
+			suma2 += i;
+		}
+	}
+
+	cout << "Suma skaiciu kurie dalinasi is 4: " << suma2 << endl;
+
+	// 4 pavyzdys
+
+	for (int i = 0; i < 15; i++)
+	{
+		if (i % 2 == 0 && i != 0)
+		{
+			cout << i << endl;
+		}
+	}
+
+	return 0;
+}
