0c23bfee6b0f1a6294a8fc8e4437e7fef03e5be6
Ieva <ieva.jaksaityte@gmail.com>
2016-11-09 15:30:08 +0200

    perdavinau aplankus, kad viskas būtų iš eilės

---
+++ 08 - Aritmetiniai veiksmai (2)/Source.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 10,
+		b = 15;
+
+	int c = (10 + 15) * 2;
+
+	int d = (5 + 9 * 2) - 5 * 3;
+
+	double suma = a + b + 45.454;
+
+	int suma_suma = 14 + 56 + 5454 + 544548;
+
+	return 0;
+}
---
+++ 09 - Informacijos išvedimas į ekraną/Source.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "cia yra isvedimo tekstas";
+	cout << endl; // end line
+
+	cout << "cia yra kitas tekstas" << endl;
+
+	int a = 10;
+	int b = 5;
+
+	cout << "mano skaicius a = " << a << endl
+		<< "mano skaicius b = " << b << endl;
+
+	cout << "vienas" << endl;
+	cout << "kitas" << endl;
+	cout << "trecias" << endl;
+
+	cout << "vienas" << endl << "kitas" << endl
+		<< "trecias" << endl;
+	
+	cout << a << " + " << b << " = " << a + b << endl;
+
+	cout << "5 + 5 = " << 5 + 5 << endl;
+
+	return 0;
+}
