259be4af7ff89e9b043641a7d52670e74751108e
Ieva <ieva.jaksaityte@gmail.com>
2016-11-11 16:22:24 +0200

    pridėti pamokų kodai

---
+++ 22 - Do while ciklas/Source.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int skaicius;
+
+	do
+	{
+		cout << "pasirinkite skaiciu esanti tarp [0-100]: ";
+		cin >> skaicius;
+
+	} while (skaicius < 0 || skaicius > 100);
+
+	cout << "ivestas skaicius: " << skaicius << endl;
+
+	return 0;
+}
