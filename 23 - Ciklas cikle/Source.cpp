4ce984c407d9231b3941dc518c1a2817e6c3217e
Ieva <ieva.jaksaityte@gmail.com>
2016-11-13 13:52:34 +0200

    pridėti pamokų kodai ir užduotys

---
+++ 23 - Ciklas cikle/Source.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "i: " << i << endl;
+
+		for (int j = 0; j < 10; j++)
+		{
+			cout << "j: " << j << " ";
+		}
+
+		cout << endl;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			cout << i << j << " ";
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << i << ": ";
+		for (int j = 0; j < 5; j++)
+		{
+			cout << i * j << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
