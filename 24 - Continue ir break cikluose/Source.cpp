bd8bbe3b6e22c2dc757fc492dabf61957e948c59
Ieva <ieva.jaksaityte@gmail.com>
2016-11-15 16:25:38 +0200

    pridėti pamokų kodai

---
+++ 24 - Continue ir break cikluose/Source.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i = 0;
+	while (true)
+	{
+		i++;
+		if (i % 2 == 0)
+		{
+			continue;
+		}
+		if (i == 11)
+		{
+			break;
+		}
+		cout << i << endl;
+	}
+
+	cout << "kazkoks tekstas" << endl;
+
+	return 0;
+}
