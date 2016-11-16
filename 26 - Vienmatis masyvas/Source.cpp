1053fae0c452cc3b3d938553932d454587ed0d41
Ieva <ieva.jaksaityte@gmail.com>
2016-11-16 22:42:18 +0200

    pridėtas pamokos kodas ir užduotys

---
+++ 26 - Vienmatis masyvas/Source.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 8, b, c, d, e, f;
+
+	int skaiciai[10] = { 10, 5, 9, 3, 5 };
+	cout << skaiciai[0] << endl;
+
+	int pazymiai[10];
+	pazymiai[0] = 20;
+	pazymiai[1] = 12;
+	cout << pazymiai[1] << endl;
+
+	return 0;
+}
