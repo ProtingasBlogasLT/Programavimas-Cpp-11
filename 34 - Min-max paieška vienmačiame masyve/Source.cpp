dfbad014f1ec66d9d6033f8a6377bceb4df58e02
Ieva <ieva.jaksaityte@gmail.com>
2016-12-12 01:42:38 +0200

    pridėtas pamokos kodas ir užduotis

---
+++ 34 - Min-max paieška vienmačiame masyve/Source.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int skaiciai[] = { 14, 5, 6, 1, 31 };
+	int kiek = 5;
+
+	int maziausias = skaiciai[0];
+	int indeksas = 0;
+
+	for (int i = 1; i < kiek; i++)
+	{
+		if (skaiciai[i] < maziausias)
+		{
+			maziausias = skaiciai[i];
+			indeksas = i;
+		}
+	}
+
+	cout << maziausias << endl;
+
+	return 0;
+}
