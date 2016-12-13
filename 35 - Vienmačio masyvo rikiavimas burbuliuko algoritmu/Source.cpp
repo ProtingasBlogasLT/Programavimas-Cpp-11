183aaf265509d7d81b9c6438de9eec87a0aa874d
Ieva <ieva.jaksaityte@gmail.com>
2016-12-13 02:42:51 +0200

    pridėtas pamokos kodas

---
+++ 35 - Vienmačio masyvo rikiavimas burbuliuko algoritmu/Source.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int skaiciai[] = { 14, 5, 6, 1, 31 };
+	int kiek = 5;
+
+	for (int i = 0; i < kiek; i++)
+	{
+		cout << skaiciai[i] << " ";
+	}
+	cout << endl;
+
+	bool rikiuoti = true;
+	while (rikiuoti)
+	{
+		rikiuoti = false;
+
+		for (int i = 0; i < kiek - 1; i++)
+		{
+			if (skaiciai[i] > skaiciai[i + 1])
+			{
+				swap(skaiciai[i], skaiciai[i + 1]);
+				rikiuoti = true;
+			}
+		}
+	}
+
+	for (int i = 0; i < kiek; i++)
+	{
+		cout << skaiciai[i] << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
