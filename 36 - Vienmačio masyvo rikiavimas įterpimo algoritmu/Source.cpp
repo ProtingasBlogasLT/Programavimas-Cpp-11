6b02b318868766a115077c2a7f259fbde80faa00
Ieva <ieva.jaksaityte@gmail.com>
2016-12-13 17:58:27 +0200

    pridėtas programos kodas

---
+++ 36 - Vienmačio masyvo rikiavimas įterpimo algoritmu/Source.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int skaiciai[] = { 14, 5, 1, 12, 31 };
+	int kiek = 5;
+
+	for (int i = 0; i < kiek; i++)
+	{
+		cout << skaiciai[i] << " ";
+	}
+	cout << endl;
+
+	int j;
+	for (int i = 1; i < kiek; i++)
+	{
+		j = i;
+		while (j > 0 && skaiciai[j] < skaiciai[j - 1])
+		{
+			swap(skaiciai[j], skaiciai[j - 1]);
+			j--;
+		}
+	}
+
+	for (int i = 0; i < kiek; i++)
+	{
+		cout << skaiciai[i] << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
