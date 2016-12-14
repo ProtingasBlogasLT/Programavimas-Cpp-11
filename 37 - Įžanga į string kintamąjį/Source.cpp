30738a9570737aba92cc9cedc7e7934b54cbb7d2
Ieva <ieva.jaksaityte@gmail.com>
2016-12-14 23:32:46 +0200

    pridėtas pamokos kodas ir užduotis

---
+++ 37 - Įžanga į string kintamąjį/Source.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string simboliai = "sdg4d5 4df5 235@#$ %^&*)";
+	cout << simboliai << endl;
+
+	string vardas, pavarde;
+	cout << "iveskite varda ir pavarde: ";
+	cin >> vardas >> pavarde;
+	cout << vardas << endl;
+	cout << pavarde << endl;
+
+	string sakinys;
+	cout << "iveskite norima sakini: ";
+	//cin >> ws; // white space // pirmas budas
+	cin.ignore(); // antras budas
+	getline(cin, sakinys);
+	cout << sakinys << endl;
+
+	return 0;
+}
