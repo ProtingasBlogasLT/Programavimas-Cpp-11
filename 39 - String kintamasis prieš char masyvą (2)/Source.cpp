194f788227617b7fd77587052755b0d2414a0fa9
Ieva <ieva.jaksaityte@gmail.com>
2016-12-16 21:27:14 +0200

    pridėti pamokų kodai ir užduotys

---
+++ 39 - String kintamasis prieš char masyvą (2)/Source.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	// ISVEDIMAS
+
+	char ch[50] = "sdhdg";
+	int chkiek = 5;
+	string str = "jsdbdg";
+
+	cout << ch << endl;
+	cout << str << endl;
+
+	for (int i = 0; i < chkiek; i++)
+	{
+		cout << "[" << ch[i] << "]";
+	}
+	cout << endl;
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		cout << "[" << str[i] << "]";
+	}
+	cout << endl;
+
+	// KONKRECIOS RAIDES PASIEKIMAS
+
+	cout << ch[0] << endl;
+	cout << ch[chkiek - 1] << endl;
+	cout << str[0] << endl;
+	cout << str.at(0) << endl;
+	cout << str[str.size() - 1] << endl;
+	cout << str.at(str.size() - 1) << endl;
+
+	return 0;
+}
