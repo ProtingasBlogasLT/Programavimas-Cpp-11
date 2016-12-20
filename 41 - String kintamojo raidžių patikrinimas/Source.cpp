388a244c656bd17f309d69461a0023ae6bed5ce5
Ieva <ieva.jaksaityte@gmail.com>
2016-12-20 18:47:37 +0200

    pridėtas pamokos kodas ir užduotys

---
+++ 41 - String kintamojo raidžių patikrinimas/Source.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+	string str = "Ght 44 asjSrg jds";
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (isupper(str[i]))
+		{
+			cout << str[i] << " yra didzioji raide" << endl;
+		}
+
+		if (islower(str[i]))
+		{
+			cout << str[i] << " yra mazoji raide" << endl;
+		}
+
+		if (isblank(str[i]))
+		{
+			cout << str[i] << " yra tarpas" << endl;
+		}
+
+		if (isdigit(str[i]))
+		{
+			cout << str[i] << " yra skaicius" << endl;
+		}
+	}
+
+	return 0;
+}
