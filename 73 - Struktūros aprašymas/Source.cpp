b1cb059ebeef65c953a0547e01bd82a489a85068
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2017-02-28 16:57:00 +0200

    pridėtas pamokos kodas

---
+++ 73 - Struktūros aprašymas/Source.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+struct Knyga
+{
+	string Autorius;
+	string Zanras;
+	int Metai;
+	int Puslapiai;
+	double Kaina;
+};
+
+struct Automobilis
+{
+	string Marke;
+	string Modelis;
+	int Metai;
+	int Rida;
+	double DarbinisTuris;
+	int Galia; // kw
+	string Spalva;
+};
+
+struct Kompiuteris
+{
+	double CPU;
+	int PSU; // w
+	string VaizdoPlokste;
+	string MotininePlokste;
+	int RAM;
+};
+
+struct Monitorius
+{
+	string Gamintojas;
+	string Modelis;
+	double Istrizaine;
+	int RezoliucijaPlotis;
+	int RezoliucijaAukstis;
+};
+
+int main()
+{
+
+
+	return 0;
+}
